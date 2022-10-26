6b37b280f6475cc241283ea60e693dcc55ff30cc
parkeu <pej6224@naver.com>
2022-10-26 14:18:01 +0900

    blog.post 모델/마이그레이션 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) # 제목
+    content = models.TextField() # 본문
+
+    created_at = models.DateTimeField() # 시간날짜
+
+    def __str__(self):
+        return self.title
