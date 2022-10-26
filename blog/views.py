a7f57470e161f34a25b14b1ead48de5ff1289da2
parkeu <pej6224@naver.com>
2022-10-26 16:12:36 +0900

    blog.index 뷰에 post 목록 조회 추가

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from tkinter import E
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
-    return render(request, "blog/index.html")
+    # 전체 포스팅을 가져올 준비
+    # 이 시점에서는 아직 가져오지는 않음
+    post_qs = Post.objects.all().order_by("-pk")
+    # render함수 : 일종의 장고 틀
+    return render(request, "blog/index.html", {
+        # post_qs값을 post_list라는 이름으로 참조하라
+        'post_list' : post_qs, # render함수의 세번째 인자
+    })
