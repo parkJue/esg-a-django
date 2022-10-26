af03f31ae318245c34fccafd9fa4a44a1b0046c1
parkeu <pej6224@naver.com>
2022-10-26 15:12:46 +0900

    Added updated_at to blog.Post

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30) # 제목
     content = models.TextField() # 본문
 
-    created_at = models.DateTimeField() # 시간날짜
+    created_at = models.DateTimeField(auto_now_add=True) # 시간날짜
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f'[{self.pk}] {self.title}' # f"" 문자열로 만들어줌
