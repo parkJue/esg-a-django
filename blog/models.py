abe327ec60d30fdf3c1464c5ed9e29b89f64ef33
parkeu <pej6224@naver.com>
2022-10-27 11:57:11 +0900

    blog.Post 모델에 get_absolute_url 메서드 추가

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # 시간날짜
     updated_at = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        # TODO : 향후에는 장고 URL Reverse 기능 사용해보기
+        return f"/blog/{ self.pk }/"
+    
     def __str__(self):
         return f'[{self.pk}] {self.title}' # f"" 문자열로 만들어줌
